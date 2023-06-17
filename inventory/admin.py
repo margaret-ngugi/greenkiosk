81844e089f5577089165053e0dc993b714804feb
margaret-ngugi <ngugiwairimumargaret@gmail.com>
2023-06-17 18:33:20 +0300

    changes

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-# Register your models here.
-from.models import Product
+from .models import Product
+
 class ProductAdmin(admin.ModelAdmin):
-    list_display=("name","stock","price","image","description","date_created","date_updated")
-admin.site.register(Product,ProductAdmin)  
+    list_display=("name","stock","price","date_created")
+admin.site.register(Product,ProductAdmin)    
 
 
     
